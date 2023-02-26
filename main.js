58bacc129a20cc81bc658051ca63ce7a41cb07f2
Tejasmano <126317834+Tejasmano@users.noreply.github.com>
2023-02-26 19:17:43 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+status = "";
+objects = [];
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(480, 380);
+video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
+objectname = document.getElementById("input1").value;          
+}
+
+function modelLoaded()
+{
+    console.log("model loaded");
+    status = true;
+}
+
+function draw()
+{
+image(video, 0, 0, 480, 380);
+}
