eac06c1eceb7103ccf9b6c018bcc30ab23377b22
Tejasmano <126317834+Tejasmano@users.noreply.github.com>
2023-02-27 19:23:47 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -26,4 +26,47 @@ function modelLoaded()
 function draw()
 {
 image(video, 0, 0, 480, 380);
+if(status!="")
+    {
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i<objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "status : objects detected";
+            fill("#ff0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y +15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if(objectname == objects[i].label)
+            {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("status").innerHTML = "status : object "+ objectname +" found";
+                var synth = window.speechSynthesis;
+                speak_data = "object "+ objectname +" found";
+            var utterThis = new SpeechSynthesisUtterance(speak_data);
+            synth.speak(utterThis);
+            }
+            else
+            {
+                document.getElementById("status").innerHTML = "status : object "+ objectname +" not found";
+            }
+           
+            
+        }
+    }
+}
+
+function gotResult(error, results)
+{
+if(error)
+{
+    console.log(error);
+}
+else
+{
+    console.log(results);
+    objects = results;
+}
 }
